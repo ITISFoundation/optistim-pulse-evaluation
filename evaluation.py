69b9023abea1fb453ae710cd3b7daa9dfe2d03af
Javier Garcia Ordonez <ordonez@zmt.swiss>
2025-01-14 15:45:06 +0100

    make local import available

--- evaluation.py
+++ evaluation.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(__file__))
+
 from get_pulse import get_pulse
 from get_pulse import SEGMENT_PW, DURATION  ## overwrite if necessary
 
--- get_pulse.py
+++ get_pulse.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(__file__))
+
 from stimulation_pulse import StimulationPulse
 import numpy as np
 
