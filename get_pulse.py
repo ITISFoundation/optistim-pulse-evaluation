50ff5e5f4bcd488a22650a47f52963d04d7af9a9
Javier Garcia Ordonez <56032114+JavierGOrdonnez@users.noreply.github.com>
2025-05-14 18:26:34 +0200

    same pulse duration and PW as optistim

--- get_pulse.py
+++ get_pulse.py
@@ -9,9 +9,9 @@ import numpy as np
 ## therefore, keep using local file
 from stimulation_pulse import StimulationPulse
 
-SEGMENT_PW = 10e-3  # Width, in ms, of 1 pulse segment
+SEGMENT_PW = 15e-3  # Width, in ms, of 1 pulse segment
 DST = int(1 / SEGMENT_PW)  # Points per unit of time (ms)
-DURATION = 0.2  # Duration of the pulse, in ms
+DURATION = 0.24  # Duration of the pulse, in ms
 NVARS = int(DURATION / SEGMENT_PW) - 1
 
 
